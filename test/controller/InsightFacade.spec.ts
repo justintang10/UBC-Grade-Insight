954d3fd58bb98d07b8efd91ec48b1f43bb196bb5
csimms <cmsimms109@gmail.com>
2024-09-19 14:54:51 -0700

    move over c0 test suite + files, change lint to lint:check in package.json to prevent error on build

--- test/controller/InsightFacade.spec.ts
+++ test/controller/InsightFacade.spec.ts
@@ -1,4 +1,12 @@
-import { IInsightFacade, InsightDatasetKind, InsightError, InsightResult } from "../../src/controller/IInsightFacade";
+import {
+	IInsightFacade,
+	//InsightDataset,
+	InsightDatasetKind,
+	InsightError,
+	NotFoundError,
+	//InsightResult,
+	ResultTooLargeError,
+} from "../../src/controller/IInsightFacade";
 import InsightFacade from "../../src/controller/InsightFacade";
 import { clearDisk, getContentFromArchives, loadTestQuery } from "../TestUtil";
 
@@ -19,10 +27,33 @@ describe("InsightFacade", function () {
 
 	// Declare datasets used in tests. You should add more datasets like this!
 	let sections: string;
+	let singleCourseDataSet: string;
+	let emptyDataset: string;
+	let smallDataset: string;
+	let emptyCourseDataset: string;
+	let imageFile: string;
+	let invalidCourseDataset: string;
+	let singleCourseMissingField: string;
+	let singleCourseSingleSection: string;
+	let wrongDirectoryName: string;
+
+	const smallDatasetNumRows = 103;
+	const singleCourseDatasetNumRows = 22;
+	const two = 2;
 
 	before(async function () {
 		// This block runs once and loads the datasets.
 		sections = await getContentFromArchives("pair.zip");
+		singleCourseDataSet = await getContentFromArchives("singleCourseDataset.zip");
+		emptyDataset = await getContentFromArchives("emptyDataset.zip");
+		smallDataset = await getContentFromArchives("smallDataset.zip");
+
+		emptyCourseDataset = await getContentFromArchives("emptyCourseDataset.zip");
+		imageFile = await getContentFromArchives("image.zip");
+		invalidCourseDataset = await getContentFromArchives("invalidCourseDataset.zip");
+		singleCourseMissingField = await getContentFromArchives("singleCourseMissingField.zip");
+		singleCourseSingleSection = await getContentFromArchives("singleCourseSingleSection.zip");
+		wrongDirectoryName = await getContentFromArchives("wrongDirectoryName.zip");
 
 		// Just in case there is anything hanging around from a previous run of the test suite
 		await clearDisk();
@@ -41,13 +72,344 @@ describe("InsightFacade", function () {
 			await clearDisk();
 		});
 
-		it("should reject with  an empty dataset id", async function () {
-			try {
-				await facade.addDataset("", sections, InsightDatasetKind.Sections);
-			} catch (err) {
-				expect(err).to.be.instanceOf(InsightError);
-			}
-			expect.fail("Should have thrown above.");
+		/*
+		AddDataset ID tests
+		 */
+
+		it("should reject with empty dataset id", async function () {
+			return facade
+				.addDataset("", singleCourseDataSet, InsightDatasetKind.Sections)
+				.then((result) => {
+					expect.fail("should not have succeeded: " + result);
+				})
+				.catch((err) => {
+					expect(err).to.be.instanceOf(InsightError);
+				});
+		});
+
+		it("should reject with a dataset id containing only whitespace", async function () {
+			return facade
+				.addDataset("      ", singleCourseDataSet, InsightDatasetKind.Sections)
+				.then((result) => {
+					expect.fail("should not have succeeded: " + result);
+				})
+				.catch((err) => {
+					expect(err).to.be.instanceOf(InsightError);
+				});
+		});
+
+		it("should reject with a dataset id containing an underscore", async function () {
+			return facade
+				.addDataset("dataset_id", singleCourseDataSet, InsightDatasetKind.Sections)
+				.then((result) => {
+					expect.fail("should not have succeeded: " + result);
+				})
+				.catch((err) => {
+					expect(err).to.be.instanceOf(InsightError);
+				});
+		});
+
+		it("should reject with a dataset id containing multiple underscores", async function () {
+			return facade
+				.addDataset("_under_scores_id_", singleCourseDataSet, InsightDatasetKind.Sections)
+				.then((result) => {
+					expect.fail("should not have succeeded: " + result);
+				})
+				.catch((err) => {
+					expect(err).to.be.instanceOf(InsightError);
+				});
+		});
+
+		it("should successfully add a dataset with a permitted id", async function () {
+			return facade
+				.addDataset("datasetid", singleCourseDataSet, InsightDatasetKind.Sections)
+				.then((result) => {
+					expect(result).to.have.members(["datasetid"]);
+				})
+				.catch((err) => {
+					expect.fail("should not have failed: " + err);
+				});
+		});
+
+		/*
+		AddDataset Content Validation Tests
+		 */
+
+		it("should reject with an empty dataset", async function () {
+			return facade
+				.addDataset("datasetid", emptyDataset, InsightDatasetKind.Sections)
+				.then((result) => {
+					expect.fail("should not have succeeded: " + result);
+				})
+				.catch((err) => {
+					expect(err).to.be.instanceOf(InsightError);
+				});
+		});
+
+		it("should pass with a single course dataset", async function () {
+			return facade
+				.addDataset("datasetid", singleCourseDataSet, InsightDatasetKind.Sections)
+				.then((result) => {
+					expect(result).to.have.members(["datasetid"]);
+				})
+				.catch((err) => {
+					expect.fail("should not have failed: " + err);
+				});
+		});
+
+		it("should pass with a small dataset", async function () {
+			return facade
+				.addDataset("datasetid", smallDataset, InsightDatasetKind.Sections)
+				.then((result) => {
+					expect(result).to.have.members(["datasetid"]);
+				})
+				.catch((err) => {
+					expect.fail("should not have failed: " + err);
+				});
+		});
+
+		it("should fail with an invalid zip file", async function () {
+			return facade
+				.addDataset("datasetid", imageFile, InsightDatasetKind.Sections)
+				.then((result) => {
+					expect.fail("should not have succeeded: " + result);
+				})
+				.catch((err) => {
+					expect(err).to.be.instanceOf(InsightError);
+				});
+		});
+
+		it("should fail with an invalid course file", async function () {
+			return facade
+				.addDataset("datasetid", invalidCourseDataset, InsightDatasetKind.Sections)
+				.then((result) => {
+					expect.fail("should not have succeeded: " + result);
+				})
+				.catch((err) => {
+					expect(err).to.be.instanceOf(InsightError);
+				});
+		});
+
+		it("should fail with an empty course file", async function () {
+			return facade
+				.addDataset("datasetid", emptyCourseDataset, InsightDatasetKind.Sections)
+				.then((result) => {
+					expect.fail("should not have succeeded: " + result);
+				})
+				.catch((err) => {
+					expect(err).to.be.instanceOf(InsightError);
+				});
+		});
+
+		it("should fail if one of the course sections is missing a needed field", async function () {
+			return facade
+				.addDataset("datasetid", singleCourseMissingField, InsightDatasetKind.Sections)
+				.then((result) => {
+					expect.fail("should not have succeeded: " + result);
+				})
+				.catch((err) => {
+					expect(err).to.be.instanceOf(InsightError);
+				});
+		});
+
+		it("should pass for a dataset with a single section", async function () {
+			return facade
+				.addDataset("datasetid", singleCourseSingleSection, InsightDatasetKind.Sections)
+				.then((result) => {
+					expect(result).to.have.members(["datasetid"]);
+				})
+				.catch((err) => {
+					expect.fail("should not have failed: " + err);
+				});
+		});
+
+		it("should fail if the directory is not named courses", async function () {
+			return facade
+				.addDataset("datasetid", wrongDirectoryName, InsightDatasetKind.Sections)
+				.then((result) => {
+					expect.fail("should not have succeeded: " + result);
+				})
+				.catch((err) => {
+					expect(err).to.be.instanceOf(InsightError);
+				});
+		});
+
+		it("should actually add the dataset", async function () {
+			await facade.addDataset("datasetid", singleCourseSingleSection, InsightDatasetKind.Sections);
+
+			return facade
+				.listDatasets()
+				.then((result) => {
+					expect(result.length).to.equal(1);
+					expect(result[0].id).to.equal("datasetid");
+					expect(result[0].kind).to.equal(InsightDatasetKind.Sections);
+					expect(result[0].numRows).to.equal(1);
+				})
+				.catch((err) => {
+					expect.fail("should not have failed: " + err);
+				});
+		});
+	});
+
+	describe("RemoveDataset", function () {
+		beforeEach(async function () {
+			// This section resets the insightFacade instance
+			// This runs before each test
+			await clearDisk();
+			facade = new InsightFacade();
+		});
+
+		afterEach(async function () {
+			// This section resets the data directory (removing any cached data)
+			// This runs after each test, which should make each test independent of the previous one
+			await clearDisk();
+		});
+
+		/*
+		RemoveDataset ID validation
+		 */
+
+		it("should fail to remove with an empty dataset id", async function () {
+			await facade.addDataset("testdata", smallDataset, InsightDatasetKind.Sections);
+
+			return facade
+				.removeDataset("")
+				.then((result) => {
+					expect.fail("should not have succeeded: " + result);
+				})
+				.catch((err) => {
+					expect(err).to.be.instanceOf(InsightError);
+				});
+		});
+
+		it("should fail to remove with a dataset id containing only whitespace", async function () {
+			await facade.addDataset("testdata", smallDataset, InsightDatasetKind.Sections);
+
+			return facade
+				.removeDataset("      ")
+				.then((result) => {
+					expect.fail("should not have succeeded: " + result);
+				})
+				.catch((err) => {
+					expect(err).to.be.instanceOf(InsightError);
+				});
+		});
+
+		it("should fail to remove with a dataset id containing an underscore", async function () {
+			await facade.addDataset("testdata", smallDataset, InsightDatasetKind.Sections);
+
+			return facade
+				.removeDataset("test_data")
+				.then((result) => {
+					expect.fail("should not have succeeded: " + result);
+				})
+				.catch((err) => {
+					expect(err).to.be.instanceOf(InsightError);
+				});
+		});
+
+		/*
+		RemoveDataset removal behaviour validation
+		 */
+
+		it("should successfully remove with a permitted dataset id", async function () {
+			await facade.addDataset("testdata", smallDataset, InsightDatasetKind.Sections);
+
+			return facade
+				.removeDataset("testdata")
+				.then((result) => {
+					expect(result).to.equal("testdata");
+				})
+				.catch((err) => {
+					expect.fail("should not have failed: " + err);
+				});
+		});
+
+		it("should fail in the case where a dataset is removed twice sequentially", async function () {
+			await facade.addDataset("testdata", smallDataset, InsightDatasetKind.Sections);
+
+			await facade.removeDataset("testdata");
+
+			return facade
+				.removeDataset("testdata")
+				.then((result) => {
+					expect.fail("should not have succeeded: " + result);
+				})
+				.catch((err) => {
+					expect(err).to.be.instanceOf(NotFoundError);
+				});
+		});
+
+		it("should fail to remove a dataset that is not there", async function () {
+			return facade
+				.removeDataset("testdata")
+				.then((result) => {
+					expect.fail("should not have succeeded: " + result);
+				})
+				.catch((err) => {
+					expect(err).to.be.instanceOf(NotFoundError);
+				});
+		});
+	});
+
+	describe("ListDatasets", function () {
+		beforeEach(function () {
+			// This section resets the insightFacade instance
+			// This runs before each test
+			facade = new InsightFacade();
+		});
+
+		afterEach(async function () {
+			// This section resets the data directory (removing any cached data)
+			// This runs after each test, which should make each test independent of the previous one
+			await clearDisk();
+		});
+
+		it("should properly return details of small dataset when added", async function () {
+			await facade.addDataset("testdata", smallDataset, InsightDatasetKind.Sections);
+
+			return facade
+				.listDatasets()
+				.then((result) => {
+					expect(result.length).to.equal(1);
+					expect(result[0].id).to.equal("testdata");
+					expect(result[0].kind).to.equal(InsightDatasetKind.Sections);
+					expect(result[0].numRows).to.equal(smallDatasetNumRows);
+				})
+				.catch((err) => {
+					expect.fail("should not have failed: " + err);
+				});
+		});
+
+		it("should properly return details of two datasets when added", async function () {
+			await facade.addDataset("testdata", smallDataset, InsightDatasetKind.Sections);
+			await facade.addDataset("testdata2", singleCourseDataSet, InsightDatasetKind.Sections);
+			return facade
+				.listDatasets()
+				.then((result) => {
+					expect(result.length).to.equal(two);
+					expect(result[0].id).to.equal("testdata");
+					expect(result[0].kind).to.equal(InsightDatasetKind.Sections);
+					expect(result[0].numRows).to.equal(smallDatasetNumRows);
+
+					expect(result[1].id).to.equal("testdata2");
+					expect(result[1].kind).to.equal(InsightDatasetKind.Sections);
+					expect(result[1].numRows).to.equal(singleCourseDatasetNumRows);
+				})
+				.catch((err) => {
+					expect.fail("should not have failed: " + err);
+				});
+		});
+
+		it("should properly return an empty array when no datasets have been added", async function () {
+			return facade
+				.listDatasets()
+				.then((result) => {
+					expect(result.length).to.equal(0);
+				})
+				.catch((err) => {
+					expect.fail("Should not have failed: " + err);
+				});
 		});
 	});
 
@@ -67,23 +429,28 @@ describe("InsightFacade", function () {
 			}
 			// Destructuring assignment to reduce property accesses
 			const { input, expected, errorExpected } = await loadTestQuery(this.test.title);
-			let result: InsightResult[] = []; // dummy value before being reassigned
-			try {
-				result = await facade.performQuery(input);
-			} catch (err) {
-				if (!errorExpected) {
-					expect.fail(`performQuery threw unexpected error: ${err}`);
-				}
-				// TODO: replace this failing assertion with your assertions. You will need to reason about the code in this function
-				// to determine what to put here :)
-				return expect.fail("Write your assertion(s) here.");
-			}
-			if (errorExpected) {
-				expect.fail(`performQuery resolved when it should have rejected with ${expected}`);
-			}
-			// TODO: replace this failing assertion with your assertions. You will need to reason about the code in this function
-			// to determine what to put here :)
-			return expect.fail("Write your assertion(s) here.");
+
+			return facade
+				.performQuery(input)
+				.then((result) => {
+					if (errorExpected) {
+						expect.fail("performQuery resolved when it should have rejected with " + expected);
+					}
+
+					expect(result).to.deep.equal(expected);
+				})
+				.catch((err) => {
+					if (!errorExpected) {
+						expect.fail("performQuery threw unexpected error: " + err);
+					}
+
+					//make sure returned error is correct
+					if (expected === "InsightError") {
+						expect(err).to.be.instanceOf(InsightError);
+					} else {
+						expect(err).to.be.instanceOf(ResultTooLargeError);
+					}
+				});
 		}
 
 		before(async function () {
@@ -93,6 +460,7 @@ describe("InsightFacade", function () {
 			// Will *fail* if there is a problem reading ANY dataset.
 			const loadDatasetPromises: Promise<string[]>[] = [
 				facade.addDataset("sections", sections, InsightDatasetKind.Sections),
+				facade.addDataset("smalldataset", smallDataset, InsightDatasetKind.Sections),
 			];
 
 			try {
@@ -108,7 +476,39 @@ describe("InsightFacade", function () {
 
 		// Examples demonstrating how to test performQuery using the JSON Test Queries.
 		// The relative path to the query file must be given in square brackets.
+		it("[valid/andWithOneFilter.json] SELECT avg WHERE avg > 98 AND", checkQuery);
+		it("[valid/AndWithOnlyOneStatement.json] SELECT dept, id, avg WHERE id = 411", checkQuery);
+		it(
+			"[valid/bigQuery.json] SELECT dept, id, avg, instructor WHERE sections_avg > 90 AND sections_dept = ch* OR sections_avg = 67 AND NOT sections_pass < 60 OR sections_instructor = si* AND sections_id = 100 AND NOT sections_avg < 63 ORDER BY avg",
+			checkQuery
+		);
+		it("[valid/emptyResults.json] SELECT dept, id, avg WHERE avg > 100", checkQuery);
+		it("[valid/exactQueryAllFields.json] exact query for one row", checkQuery);
+		it("[valid/largeResult.json] SELECT dept, avg WHERE avg > 88.75 OR avg < 51.5", checkQuery);
+		it("[valid/orderByStringField.json] SELECT avg, dept WHERE avg > 95 ORDER BY dept", checkQuery);
+		it("[valid/queryWithNot.json] SELECT dept, avg WHERE NOT avg > 55 AND dept = math", checkQuery);
 		it("[valid/simple.json] SELECT dept, avg WHERE avg > 97", checkQuery);
+		it("[valid/validByInstructor.json] SELECT dept, id, avg WHERE instructor = belleville, patrice", checkQuery);
+		it(
+			"[valid/validOrAnd.json] SELECT dept, id, avg WHERE dept = apsc AND avg > 90, OR dept = mech AND avg < 60",
+			checkQuery
+		);
+		it("[valid/validWildcard.json] SELECT dept, id, avg WHERE dept = cpsc AND id = 5*", checkQuery);
+
+		//invalids
+		it("[invalid/avgAsString.json] SELECT avg WHERE avg = '43'", checkQuery);
+		it("[invalid/avgQueriedWithIs.json] SELECT avg WHERE avg IS 80", checkQuery);
+		it("[invalid/badQueryGrammar.json] SELECT avg WHERE AND", checkQuery);
+		it("[invalid/badQueryGrammar2.json] SELECT avg WHERE IS");
+		it("[invalid/emptyColumnsList.json] SELECT WHERE avg > 97", checkQuery);
+		it("[invalid/emptyIsStatement.json] SELECT dept, id, WHERE IS", checkQuery);
+		it("[invalid/idAsInt.json] SELECT avg WHERE id IS 400", checkQuery);
+		it("[invalid/idQueriedWithEq.json] SELECT avg where id = 400");
 		it("[invalid/invalid.json] Query missing WHERE", checkQuery);
+		it("[invalid/invalidWildcard.json] SELECT dept, id, avg WHERE dept = c*sc AND id = 5*", checkQuery);
+		it("[invalid/matchAll.json] SELECT dept, id, WHERE", checkQuery);
+		it("[invalid/missingOptions.json] SELECT WHERE avg > 97 NO OPTIONS", checkQuery);
+		it("[invalid/referencesMultipleDatasets.json] SELECT dept, id, avg WHERE dept = engl AND avg > 60", checkQuery);
+		it("[invalid/resultTooLarge.json] SELECT avg WHERE avg > 88.74", checkQuery);
 	});
 });
