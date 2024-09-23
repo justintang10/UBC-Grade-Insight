b0920905c0eb44270f30a32d97e79dff4172bf21
csimms <cmsimms109@gmail.com>
2024-09-23 15:54:37 -0700

    also this

--- src/models/dataset.ts
+++ src/models/dataset.ts
@@ -1,20 +1,21 @@
 import {InsightDataset, InsightDatasetKind} from "../controller/IInsightFacade";
+import {Section} from "./section";
 
 export class Dataset implements InsightDataset {
 
-	private readonly sections: Dataset[];
+	private readonly sections: Section[];
 	public readonly id: string;
 	public readonly kind: InsightDatasetKind;
 	public readonly numRows: number;
 
-	constructor(sections: Dataset[], id: string, kind: InsightDatasetKind, numRows: number) {
+	constructor(sections: Section[], id: string, kind: InsightDatasetKind, numRows: number) {
 		this.sections = sections;
 		this.id = id;
 		this.kind = kind;
 		this.numRows = numRows;
 	}
 
-	public getDataset(): Dataset[] {
+	public getDataset(): Section[] {
 		return this.sections;
 	}
 }
