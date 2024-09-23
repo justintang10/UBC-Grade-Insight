e533b44e79798db6002ec68c075149af0e77dcde
csimms <cmsimms109@gmail.com>
2024-09-23 15:53:53 -0700

    fix Dataset class to implement InsightDataset, an oversight initially

--- src/models/dataset.ts
+++ src/models/dataset.ts
@@ -1,9 +1,17 @@
+import {InsightDataset, InsightDatasetKind} from "../controller/IInsightFacade";
 
-export class Dataset {
+export class Dataset implements InsightDataset {
+
 	private readonly sections: Dataset[];
+	public readonly id: string;
+	public readonly kind: InsightDatasetKind;
+	public readonly numRows: number;
 
-	constructor(sections: Dataset[]) {
+	constructor(sections: Dataset[], id: string, kind: InsightDatasetKind, numRows: number) {
 		this.sections = sections;
+		this.id = id;
+		this.kind = kind;
+		this.numRows = numRows;
 	}
 
 	public getDataset(): Dataset[] {
