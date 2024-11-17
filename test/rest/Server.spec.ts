5b7129342e3d81fdaa659d69b5993fdf703cc179
csimms <cmsimms109@gmail.com>
2024-11-16 22:58:08 -0800

    add a bunch more c3 tests + add some stuff in the beforeall clause to make the tests easier to work with (less setup work if tests fail)

--- test/rest/Server.spec.ts
+++ test/rest/Server.spec.ts
@@ -8,10 +8,12 @@ import { getContentFromArchives } from "../TestUtil";
 describe("Facade C3", function () {
 	let server: Server;
 	let sectionsBytes: Buffer;
+	let sectionsBytesInvalid: Buffer;
 
 	before(async function () {
 		const sections = await getContentFromArchives("pair.zip");
 		sectionsBytes = Buffer.from(sections, "base64");
+		sectionsBytesInvalid = Buffer.from("invalid dataset", "base64");
 
 		try {
 			//start server (random unused port)
@@ -21,6 +23,15 @@ describe("Facade C3", function () {
 		} catch (err) {
 			Log.error("failed to start server");
 			expect.fail("Failed to start C3 server: " + err);
+		}
+
+		//remove dataset that gets added (in case test suite failed to complete & remove)
+		const SERVER_URL = "localhost:49155";
+		const ENDPOINT_URL = "/dataset/sections";
+		try {
+			await request(SERVER_URL).delete(ENDPOINT_URL);
+		} catch (err) {
+			Log.error(err);
 		}
 	});
 
@@ -56,6 +67,54 @@ describe("Facade C3", function () {
 					Log.info(res.body.result);
 					expect(res.body.result).to.have.members(["sections"]);
 					expect(res.status).to.be.equal(StatusCodes.OK);
+				})
+				.catch(function () {
+					// some logging here please!
+					expect.fail();
+				});
+		} catch (err) {
+			Log.error(err);
+			// and some more logging here!
+		}
+	});
+
+	it("PUT test for courses dataset (duplicate dataset id)", function () {
+		const SERVER_URL = "localhost:49155";
+		const ENDPOINT_URL = "/dataset/sections/sections";
+
+		try {
+			return request(SERVER_URL)
+				.put(ENDPOINT_URL)
+				.send(sectionsBytes)
+				.set("Content-Type", "application/x-zip-compressed")
+				.then(function (res: Response) {
+					// some logging here please!
+					Log.info(res.body.result);
+					expect(res.status).to.be.equal(StatusCodes.BAD_REQUEST);
+				})
+				.catch(function () {
+					// some logging here please!
+					expect.fail();
+				});
+		} catch (err) {
+			Log.error(err);
+			// and some more logging here!
+		}
+	});
+
+	it("PUT test for courses dataset (invalid dataset)", function () {
+		const SERVER_URL = "localhost:49155";
+		const ENDPOINT_URL = "/dataset/sections/sectionsinvalid";
+
+		try {
+			return request(SERVER_URL)
+				.put(ENDPOINT_URL)
+				.send(sectionsBytesInvalid)
+				.set("Content-Type", "application/x-zip-compressed")
+				.then(function (res: Response) {
+					// some logging here please!
+					Log.info(res.body.result);
+					expect(res.status).to.be.equal(StatusCodes.BAD_REQUEST);
 				})
 				.catch(function () {
 					// some logging here please!
@@ -127,6 +186,36 @@ describe("Facade C3", function () {
 		}
 	});
 
+	it("POST test for courses dataset (invalid query)", function () {
+		const SERVER_URL = "localhost:49155";
+		const ENDPOINT_URL = "/query";
+		const query = {
+			OPTIONS: {
+				COLUMNS: ["sections_dept", "sections_avg"],
+				ORDER: "sections_avg",
+			},
+		};
+
+		try {
+			return request(SERVER_URL)
+				.post(ENDPOINT_URL)
+				.send(query)
+				.set("Content-Type", "application/json")
+				.then(function (res: Response) {
+					// some logging here please!
+					Log.info(res.body.result);
+					expect(res.status).to.be.equal(StatusCodes.BAD_REQUEST);
+				})
+				.catch(function () {
+					// some logging here please!
+					expect.fail();
+				});
+		} catch (err) {
+			Log.error(err);
+			// and some more logging here!
+		}
+	});
+
 	it("DELETE test for courses dataset", function () {
 		const SERVER_URL = "localhost:49155";
 		const ENDPOINT_URL = "/dataset/sections";
@@ -138,6 +227,50 @@ describe("Facade C3", function () {
 					// some logging here please!
 					Log.info(res.body.result);
 					expect(res.status).to.be.equal(StatusCodes.OK);
+				})
+				.catch(function () {
+					// some logging here please!
+					expect.fail();
+				});
+		} catch (err) {
+			Log.error(err);
+			// and some more logging here!
+		}
+	});
+
+	it("DELETE test for courses dataset (dataset not found)", function () {
+		const SERVER_URL = "localhost:49155";
+		const ENDPOINT_URL = "/dataset/sections";
+
+		try {
+			return request(SERVER_URL)
+				.delete(ENDPOINT_URL)
+				.then(function (res: Response) {
+					// some logging here please!
+					Log.info(res.body.result);
+					expect(res.status).to.be.equal(StatusCodes.NOT_FOUND);
+				})
+				.catch(function () {
+					// some logging here please!
+					expect.fail();
+				});
+		} catch (err) {
+			Log.error(err);
+			// and some more logging here!
+		}
+	});
+
+	it("DELETE test for courses dataset (datasetid invalid)", function () {
+		const SERVER_URL = "localhost:49155";
+		const ENDPOINT_URL = "/dataset/section_s";
+
+		try {
+			return request(SERVER_URL)
+				.delete(ENDPOINT_URL)
+				.then(function (res: Response) {
+					// some logging here please!
+					Log.info(res.body.result);
+					expect(res.status).to.be.equal(StatusCodes.BAD_REQUEST);
 				})
 				.catch(function () {
 					// some logging here please!
