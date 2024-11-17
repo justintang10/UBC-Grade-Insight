ba62bf47f1bffb23321c8f88d63da0d65c2e9c58
csimms <cmsimms109@gmail.com>
2024-11-16 23:09:39 -0800

    add c3 test for rooms dataset, and another for list

--- test/rest/Server.spec.ts
+++ test/rest/Server.spec.ts
@@ -9,11 +9,14 @@ describe("Facade C3", function () {
 	let server: Server;
 	let sectionsBytes: Buffer;
 	let sectionsBytesInvalid: Buffer;
+	let roomsBytes: Buffer;
 
 	before(async function () {
 		const sections = await getContentFromArchives("pair.zip");
+		const rooms = await getContentFromArchives("campus.zip");
 		sectionsBytes = Buffer.from(sections, "base64");
 		sectionsBytesInvalid = Buffer.from("invalid dataset", "base64");
+		roomsBytes = Buffer.from(rooms, "base64");
 
 		try {
 			//start server (random unused port)
@@ -27,9 +30,11 @@ describe("Facade C3", function () {
 
 		//remove dataset that gets added (in case test suite failed to complete & remove)
 		const SERVER_URL = "localhost:49155";
-		const ENDPOINT_URL = "/dataset/sections";
+		const COURSES_ENDPOINT_URL = "/dataset/sections";
+		const ROOMS_ENDPOINT_URL = "/dataset/rooms";
 		try {
-			await request(SERVER_URL).delete(ENDPOINT_URL);
+			await request(SERVER_URL).delete(COURSES_ENDPOINT_URL);
+			await request(SERVER_URL).delete(ROOMS_ENDPOINT_URL);
 		} catch (err) {
 			Log.error(err);
 		}
@@ -66,6 +71,31 @@ describe("Facade C3", function () {
 					// some logging here please!
 					Log.info(res.body.result);
 					expect(res.body.result).to.have.members(["sections"]);
+					expect(res.status).to.be.equal(StatusCodes.OK);
+				})
+				.catch(function () {
+					// some logging here please!
+					expect.fail();
+				});
+		} catch (err) {
+			Log.error(err);
+			// and some more logging here!
+		}
+	});
+
+	it("PUT test for rooms dataset", function () {
+		const SERVER_URL = "localhost:49155";
+		const ENDPOINT_URL = "/dataset/rooms/rooms";
+
+		try {
+			return request(SERVER_URL)
+				.put(ENDPOINT_URL)
+				.send(roomsBytes)
+				.set("Content-Type", "application/x-zip-compressed")
+				.then(function (res: Response) {
+					// some logging here please!
+					Log.info(res.body.result);
+					expect(res.body.result).to.have.members(["sections","rooms"]);
 					expect(res.status).to.be.equal(StatusCodes.OK);
 				})
 				.catch(function () {
@@ -126,7 +156,7 @@ describe("Facade C3", function () {
 		}
 	});
 
-	it("GET test for courses dataset", function () {
+	it("GET test for datasets", function () {
 		const SERVER_URL = "localhost:49155";
 		const ENDPOINT_URL = "/datasets";
 
@@ -136,7 +166,7 @@ describe("Facade C3", function () {
 				.then(function (res: Response) {
 					// some logging here please!
 					Log.info(res.body.result);
-					expect(res.body.result).to.have.length(1);
+					expect(res.body.result).to.have.length(2);
 					expect(res.status).to.be.equal(StatusCodes.OK);
 				})
 				.catch(function () {
@@ -271,6 +301,29 @@ describe("Facade C3", function () {
 					// some logging here please!
 					Log.info(res.body.result);
 					expect(res.status).to.be.equal(StatusCodes.BAD_REQUEST);
+				})
+				.catch(function () {
+					// some logging here please!
+					expect.fail();
+				});
+		} catch (err) {
+			Log.error(err);
+			// and some more logging here!
+		}
+	});
+
+	it("GET test for datasets (after removal)", function () {
+		const SERVER_URL = "localhost:49155";
+		const ENDPOINT_URL = "/datasets";
+
+		try {
+			return request(SERVER_URL)
+				.get(ENDPOINT_URL)
+				.then(function (res: Response) {
+					// some logging here please!
+					Log.info(res.body.result);
+					expect(res.body.result).to.have.length(1);
+					expect(res.status).to.be.equal(StatusCodes.OK);
 				})
 				.catch(function () {
 					// some logging here please!
