286c33fd3a8d718e8fb49c38a3c3e99f04f6f748
Munn Chai <113135503+LotsaChai@users.noreply.github.com>
2024-11-18 21:21:01 -0800

    Tested fetch and maybe plotting library?

--- frontend/public/frontend.js
+++ frontend/public/frontend.js
@@ -1,5 +1,29 @@
+import * as Plot from "https://cdn.jsdelivr.net/npm/@observablehq/plot@0.6/+esm";
+
 document.getElementById("click-me-button").addEventListener("click", handleClickMe);
 
-function handleClickMe() {
-	alert("Button Clicked!");
+async function handleClickMe() {
+	try {
+		const response = await fetch("http://localhost:4321/echo/hello");
+		if (!response.ok) {
+			throw new Error(`Response status: ${response.status}`);
+		}
+
+		const json = await response.json();
+		alert("This is a test! \n" + json["result"]);
+	} catch (e) {
+		alert("Error! \n" + e.message);
+	}
 }
+
+const plot = Plot.rectY(
+	{length: 1000},
+	Plot.binX(
+		{y: "count"},
+		{x: Math.random}
+	)
+).plot();
+
+const div = document.querySelector("#myplot");
+
+div.append(plot);
