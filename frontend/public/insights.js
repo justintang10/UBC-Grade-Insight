8be2291684403b2cf73ff33807c03958ab2da3b7
Munn Chai <113135503+LotsaChai@users.noreply.github.com>
2024-11-20 02:06:42 -0800

    Added insights for course averages and top profs

--- frontend/public/insights.js
+++ frontend/public/insights.js
@@ -68,14 +68,160 @@ async function getRequiredInfoFromDatasets() {
 		const array = deptMap[deptName];
 		array.push(courseID);
 	}
-	console.log(deptMap);
+	// console.log(deptMap);
 
 	updateInsightResultContainers();
 }
 
 function updateInsightResultContainers() {
-	const courseAveragesContainer = document.getElementById("Course Averages");
+	updateCourseAveragesContainer();
+	updateTopProfsContainer();
+}
 
+function updateCourseAveragesContainer() {
+	const deptDropdown = document.getElementById("courseAverageDeptDropdown");
+
+	for (const dept in deptMap) {
+		const option = document.createElement("option");
+		option.value = dept;
+		option.innerHTML = dept;
+		deptDropdown.appendChild(option);
+	}
+
+	const courseDropdown = document.getElementById("courseAverageCourseDropdown");
+
+	deptDropdown.addEventListener('change', function () {
+		updateCoursesDropdown(courseDropdown, deptMap[deptDropdown.value]);
+	});
+
+	deptDropdown.addEventListener('focus', function () {
+		deptDropdown.selectedIndex = -1;
+		courseDropdown.innerHTML = "";
+	});
+
+	// View insights button
+	const getInsightButton = document.getElementById("courseAverageButton");
+	getInsightButton.addEventListener('click',
+		function() {
+			getCourseAverageInsight();
+		})
+}
+
+async function getCourseAverageInsight() {
+	const deptDropdown = document.getElementById("courseAverageDeptDropdown");
+	const courseDropdown = document.getElementById("courseAverageCourseDropdown");
+
+	const deptId = deptDropdown.value;
+	const courseId = courseDropdown.value;
+
+	// Clone query, as to not modify the original
+	const query = JSON.parse(JSON.stringify(queries.getCourseAverages));
+	const datasetId = localStorage.getItem("CurrentDatasetID");
+	const datasetIdDept = datasetId + "_dept";
+	const datasetIdCourse = datasetId + "_id";
+	const is1 = {}
+	is1[datasetIdDept] = deptId;
+	const is2 = {}
+	is2[datasetIdCourse] = courseId;
+	query["WHERE"] = {
+		"AND": [
+			{
+				"IS": is1
+			},
+			{
+				"IS": is2
+			}
+		]
+	}
+
+	const response = await fetch("http://localhost:4321/query", {
+		method: "POST",
+		headers: {"Content-Type": "application/json"},
+		body: JSON.stringify(query),
+	});
+
+	const avgs = await response.json();
+	console.log("Course Averages over the years for: " + deptId + " " + courseId);
+	console.log(avgs);
+}
+
+function updateTopProfsContainer() {
+	const deptDropdown = document.getElementById("topProfsDeptDropdown");
+
+	for (const dept in deptMap) {
+		const option = document.createElement("option");
+		option.value = dept;
+		option.innerHTML = dept;
+		deptDropdown.appendChild(option);
+	}
+
+	const courseDropdown = document.getElementById("topProfsCourseDropdown");
+
+	deptDropdown.addEventListener('change', function () {
+		updateCoursesDropdown(courseDropdown, deptMap[deptDropdown.value]);
+	});
+
+	deptDropdown.addEventListener('focus', function () {
+		deptDropdown.selectedIndex = -1;
+		courseDropdown.innerHTML = "";
+	});
+
+	// View insights button
+	const getInsightButton = document.getElementById("topProfsButton");
+	getInsightButton.addEventListener('click',
+		function() {
+			getTopProfsInsight();
+		})
+}
+
+async function getTopProfsInsight() {
+	const deptDropdown = document.getElementById("topProfsDeptDropdown");
+	const courseDropdown = document.getElementById("topProfsCourseDropdown");
+
+	const deptId = deptDropdown.value;
+	const courseId = courseDropdown.value;
+
+	// Clone query, as to not modify the original
+	const query = JSON.parse(JSON.stringify(queries.getTopProfs));
+	const datasetId = localStorage.getItem("CurrentDatasetID");
+	const datasetIdDept = datasetId + "_dept";
+	const datasetIdCourse = datasetId + "_id";
+	const is1 = {}
+	is1[datasetIdDept] = deptId;
+	const is2 = {}
+	is2[datasetIdCourse] = courseId;
+	query["WHERE"] = {
+		"AND": [
+			{
+				"IS": is1
+			},
+			{
+				"IS": is2
+			}
+		]
+	}
+
+	const response = await fetch("http://localhost:4321/query", {
+		method: "POST",
+		headers: {"Content-Type": "application/json"},
+		body: JSON.stringify(query),
+	});
+
+	const avgs = await response.json();
+	console.log("Top Profs for: " + deptId + " " + courseId);
+	console.log(avgs);
+}
+
+
+function updateCoursesDropdown(dropDownElement, courseIds) {
+	dropDownElement.innerHTML = "";
+
+	for (const id of courseIds) {
+		const option = document.createElement("option");
+		option.value = id;
+		option.innerHTML = id;
+		dropDownElement.appendChild(option);
+	}
 }
 
 let deptMap = {};
@@ -111,6 +257,50 @@ const queries = {
 				localStorage.getItem("CurrentDatasetID") + "_dept"
 			],
 			"APPLY": []
+		}
+	},
+	"getCourseAverages": {
+		"WHERE": {},
+		"OPTIONS": {
+			"COLUMNS": [
+				localStorage.getItem("CurrentDatasetID") + "_year",
+				"courseAvg"
+			],
+			"ORDER": localStorage.getItem("CurrentDatasetID") + "_year"
+		},
+		"TRANSFORMATIONS": {
+			"GROUP": [
+				localStorage.getItem("CurrentDatasetID") + "_year"
+			],
+			"APPLY": [
+				{
+					"courseAvg": {
+						"AVG": localStorage.getItem("CurrentDatasetID") + "_avg"
+					}
+				}
+			]
+		}
+	},
+	"getTopProfs": {
+		"WHERE": {},
+		"OPTIONS": {
+			"COLUMNS": [
+				localStorage.getItem("CurrentDatasetID") + "_instructor",
+				"courseAvg"
+			],
+			"ORDER": "courseAvg"
+		},
+		"TRANSFORMATIONS": {
+			"GROUP": [
+				localStorage.getItem("CurrentDatasetID") + "_instructor"
+			],
+			"APPLY": [
+				{
+					"courseAvg": {
+						"AVG": localStorage.getItem("CurrentDatasetID") + "_avg"
+					}
+				}
+			]
 		}
 	}
 }
